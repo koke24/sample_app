ba6931de6628c4a292ea19d1a744aa1e30b65cfb
Koke Laast <a@b.com>
2013-10-27 09:11:28 +0100

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :test do
   gem 'selenium-webdriver', '2.0.0'
   gem 'capybara', '2.1.0'
   gem 'rubyzip', '0.9.9'
+  gem 'factory_girl_rails', '4.2.1'
 end
 
 gem 'sass-rails', '4.0.0'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,10 +9,49 @@ describe "UserPages" do
     it { should have_title(full_title(page_title)) }
   end
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    #it { should have_content(user.name) }
+    #it { should have_title(user.name) }
+    let(:heading) { user.name }
+    let(:page_title) { user.name }
+    it_should_behave_like "user pages"
+
+  end
+
   describe "signup page" do
   	before { visit signup_path }
     let(:heading) { 'Sign up' }
     let(:page_title) { 'Sign up' }
     it_should_behave_like "user pages"
   end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
 end
